91c49e9936cc33a7f46c0ee3cfba5eb637b2bb83
echeverrigroup <steven.mobil2@gmail.com>
2026-01-04 16:24:45 -0300

    Create supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://blwoktluuusvhulqjkzp.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJsd29rdGx1dXVzdmh1bHFqa3pwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUxNjY1NjcsImV4cCI6MjA4MDc0MjU2N30.Af_I--UFK1-aa0G10JQZlkNo3MquhFxPX6miDkiNRME"
+);
